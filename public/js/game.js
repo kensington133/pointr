7698adc1312d4c7d4059506e1eb7c9d45d949953
Ben Short <benshort.mouse@gmail.com>
2014-11-17 16:47:52 +0000

    Added game timer, started on the chat design and layout

--- public/js/game.js
+++ public/js/game.js
@@ -1,13 +1,40 @@
 function resize(){
 	if($('#game').length > 0){
-		$('#game, .overLay').height($(window).height());
-		$('#game, .overLay').width($(window).width());
+
+		var windowHeight = $(window).height();
+		var windowWidth = $(window).width();
+
+		$('#game').css({
+			height: (windowHeight - ($('.info').height() + $('.timer').height()) ),
+			paddingTop: $('.info').height() + $('.timer').height()
+		});
+
+		$('.overLay, .userOverlay').height(windowHeight);
+		$('.overLay, #game').width(windowWidth - $('.chatFrame').width());
+		$('.userOverlay').width(windowWidth);
+
+		$('#user').css({
+			top: windowHeight / 2,
+			left: windowWidth / 2 - $('#user').width() / 2
+		});
+
+		$('.gameOver').css({
+			top: windowHeight / 2 - $('.gameOver').height() / 2,
+			left: ((windowWidth  - $('.chatFrame').width()) / 2) - ($('.gameOver').width() / 2)
+		});
+
+		$('.info').css('left', ((windowWidth - $('.chatFrame').width()) / 2) - $('.info').width() / 2 );
+
+		$('.timer').css({
+			left: ((windowWidth - $('.chatFrame').width()) / 2) - ($('.timer').width() / 2),
+			top: $('.info').height() + 5
+		});
+
+		$('.chatFrame').height(windowHeight);
 	}
-	$('.gameOver').css({
-		top: $(window).height() / 2 - $('.gameOver').height() / 2,
-		left: $(window).width() / 2 - $('.gameOver').width() / 2
-	});
 }
+
+
 
 var paper = Raphael("game", "100%", "100%"),
 canvas = $(paper.canvas),
@@ -18,6 +45,8 @@ hover = 1;
 function init() {
 
 	resize();
+
+	var timer = new gameTimer();
 
 	var circle = paper.circle(canvas.width() / 2, canvas.height() / 2, 50);
 
@@ -38,6 +67,7 @@ function init() {
 		//players only allowed to hover once
 		if( hover === 1){
 			circle.attr('fill', '#00d419');
+			timer.start();
 			playGame(circle);
 		}
 
@@ -45,6 +75,7 @@ function init() {
 
 		//if the player falls out of the shape, game over
 		circle.mouseout(function(e){
+			timer.stop();
 			endGame(circle);
 		});
 	});
@@ -72,8 +103,38 @@ function changeScale(element){
 function endGame(element){
 	element.attr('fill', '#df2020');
 	element.stop();
+	var secondsLasted = parseInt($('.ss').text());
+	var minutesLasted = parseInt($('.mm').text());
+	var minInflection = (minutesLasted > 1)? 'minutes' : 'minute';
+	var secInflection = (secondsLasted > 1)? 'seconds' : 'second';
+
+	if(minutesLasted > 0){
+		$('.score').text('You lasted ' + minutesLasted +' '+ minInflection +' and '+ secondsLasted + ' ' + secInflection +'!');
+	} else {
+		$('.score').text('You lasted ' + secondsLasted +' '+ secInflection +'!');
+	}
+
 	$('.overLay').css('visibility','visible').hide().fadeIn();
 }
+
+function gameTimer(){
+	this.start = function(){
+		var sec = 0;
+		timerInterval = setInterval( function(){
+			$(".ss").html(pad(++sec%60));
+			$(".mm").html(pad(parseInt(sec/60,10)));
+		}, 1000);
+	}
+
+	this.stop = function(){
+		clearInterval(timerInterval);
+	}
+}
+
+function pad ( val ) {
+	return val > 9 ? val : "0" + val;
+}
+
 
 $(document).ready(function(){
 	init();
